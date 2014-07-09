d385912dda22baaaa675d24d547d084ddbaaca0c
JonathanASalomon <masterjo109@gmail.com>
2014-07-08 23:00:56 -0400

    Update search.py

--- Jonathan Salomon/07.14/02.07.14/search.py
+++ Jonathan Salomon/07.14/02.07.14/search.py
@@ -1,22 +1,26 @@
-page1=("'<a href=''http://www.yahoo.com'' /><link''<a href=''http://www.google.com'' /><link''<a href=''http://www.udacity.com'' /><link''<a href=''http;//wwww.facebook.com'' /><link''<a href=''http://www.youtube.com'' /><'")
+page1=('<a href="http://www.yahoo.com">Yahoo</a><a href="http://www.google.com"</a>Google<a href="http://www.udacity.com">Udacity</a><a href="http;//wwww.facebook.com">Facebook</a><a href="http://www.youtube.com">Youtube</a>')
+
 def get_next_target(page):
 	start_link=page.find("href")
-	start_quote=page.find("''",start_link)+2
-	end_quote=page.find("''",start_quote)
+	start_quote=page.find('"',start_link)+1
+	end_quote=page.find('"',start_quote)
 	url=page[start_quote:end_quote]
 	return url,end_quote
+
 def all_links(page):
 	links=[]
 	while(True):
 		url,end_pos=get_next_target(page)
-		if (url!="''"):
+		if (url!='"'):
 			if ("http://www.") in url:
 				links.append(url)
 				page=page[end_pos:]
 			else:
 				break
 	return links
+
 user_input1=input('Entrez votre lien: ')
+
 def search(user_input,page):
 	links=all_links(page)
 	for e in links:
@@ -24,4 +28,5 @@ def search(user_input,page):
 			return e
 		else:
 			return "Ce lien n'est pas dans la liste. Veuillez essayer google."
+
 print (search(user_input1,page1))
